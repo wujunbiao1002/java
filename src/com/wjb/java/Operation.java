43ff292c9918234ef87585cf8ade1a35b9bfcfda
Arjun <wujunbiao1002@qq.com>
2022-02-05 12:09:02 +0800

    feat： 增加变量和运算符

---
+++ src/com/wjb/java/Operation.java
@@ -0,0 +1,22 @@
+package com.wjb.java;
+
+/**
+ * <b><code>Operation</code></b>
+ * <p/>
+ * Description 运算符
+ * <p/>
+ * <b>Creation Time:</b> 2022/2/5 12:03.
+ *
+ * @author Arjun
+ * @version 1.0.0
+ * @since java 0.1.0
+ */
+public class Operation {
+
+    public static void main(String[] args) {
+        // <<左移
+        // 左移了2的n次幂
+        int i = 4;
+        System.out.println(i<<3);
+    }
+}
